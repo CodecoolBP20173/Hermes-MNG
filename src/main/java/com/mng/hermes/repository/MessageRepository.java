f88e98293b4a1acdf4a58678f8506745ecb0be33
Adam Feher <fhradm@gmail.com>
2018-07-03 09:23:11 +0200

    updated pom.xml and added RestResource annotations

--- src/main/java/com/mng/hermes/repository/MessageRepository.java
+++ src/main/java/com/mng/hermes/repository/MessageRepository.java
@@ -3,6 +3,8 @@ package com.mng.hermes.repository;
 
 import com.mng.hermes.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
+@RestResource(path = "message")
 public interface MessageRepository extends JpaRepository<Message,Integer> {
 }
--- src/main/java/com/mng/hermes/repository/UserRepository.java
+++ src/main/java/com/mng/hermes/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.mng.hermes.repository;
 
 import com.mng.hermes.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
+@RestResource(path = "user")
 public interface UserRepository extends JpaRepository<User,Integer> {
 }
