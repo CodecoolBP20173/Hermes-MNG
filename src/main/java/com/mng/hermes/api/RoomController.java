6a3a521f71917f366991517fcfcc682368c9c478
mbalazs9001 <mbalazs9001@gmail.comgit config --global user.email mbalazs9001@gmail.comgit config --global user.email mbalazs9001@gmail.com>
2018-07-03 12:53:36 +0200

    joinRoom controller method with dummy return

--- src/main/java/com/mng/hermes/api/RoomController.java
+++ src/main/java/com/mng/hermes/api/RoomController.java
@@ -1,12 +1,15 @@
 package com.mng.hermes.api;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RoomController {
 
-    @RequestMapping("/room/{id}")
-    public
+    @RequestMapping("/room/{name}")
+    public String joinRoom(@PathVariable String name) {
+        return "You have join the to room: " + name;
+    }
 
 }
