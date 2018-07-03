0100c9aea862192df85b7784856358a360564ec8
AttilaMüller <muller.attila.aron@gmail.com>
2018-07-03 13:20:02 +0200

    Added login to user controller and user dto

--- src/main/java/com/mng/hermes/api/UserController.java
+++ src/main/java/com/mng/hermes/api/UserController.java
@@ -1,7 +1,18 @@
 package com.mng.hermes.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.mng.hermes.dto.UserDTO;
+import com.mng.hermes.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public UserDTO validateUser(@RequestHeader(value="Authorization") String authorization){
+        return new UserDTO(userService.loginUser(authorization));
+    }
 }
