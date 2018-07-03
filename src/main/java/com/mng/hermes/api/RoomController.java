b2bbc7b677b2950ab25c9ae7f108ea037e906026
mbalazs9001 <mbalazs9001@gmail.comgit config --global user.email mbalazs9001@gmail.comgit config --global user.email mbalazs9001@gmail.com>
2018-07-03 13:11:27 +0200

    made rout to return all rooms

--- src/main/java/com/mng/hermes/api/RoomController.java
+++ src/main/java/com/mng/hermes/api/RoomController.java
@@ -1,11 +1,24 @@
 package com.mng.hermes.api;
 
+import com.mng.hermes.model.Room;
+import com.mng.hermes.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class RoomController {
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @RequestMapping("/rooms")
+    public List<Room> getRooms() {
+        return roomRepository.findAll();
+    }
 
     @RequestMapping("/room/{name}")
     public String joinRoom(@PathVariable String name) {
