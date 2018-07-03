0b09e41331ff85cbc7f6aea7b44af93a9d6bc83a
Adam Feher <fhradm@gmail.com>
2018-07-03 14:22:34 +0200

    MessageDTO Message Service get proper messages based on params MessageController returns messages based on request params

--- src/main/java/com/mng/hermes/api/MessageController.java
+++ src/main/java/com/mng/hermes/api/MessageController.java
@@ -1,7 +1,33 @@
 package com.mng.hermes.api;
 
+import com.mng.hermes.model.TargetType;
+import com.mng.hermes.service.MessageService;
+import com.mng.hermes.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/api")
 public class MessageController {
+    private final MessageService messageService;
+    private final UserService userService;
+
+    @Autowired
+    public MessageController(MessageService messageService, UserService userService) {
+        this.messageService = messageService;
+        this.userService = userService;
+    }
+
+    @GetMapping("/get-message")
+    public ResponseEntity getMessages(@RequestParam("target") String target, @RequestParam("type") TargetType type) {
+        int ownerId = userService.getCurrentUser().getId();
+        if ((type == null && target!=null) ||(type != null && target==null) ) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity(messageService.getMessages(target, type, ownerId),HttpStatus.OK);
+
+    }
 }
---
+++ src/main/java/com/mng/hermes/dto/MessageDTO.java
@@ -0,0 +1,17 @@
+package com.mng.hermes.dto;
+
+import com.mng.hermes.model.Message;
+
+public class MessageDTO {
+    private int id;
+    private String content;
+
+    private MessageDTO(int id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public static MessageDTO construct(Message message) {
+        return new MessageDTO(message.getId(), message.getContent());
+    }
+}
