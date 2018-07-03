c2ebebe0c2a1a9e63ee6c5271ca4c203198ac757
mbalazs9001 <mbalazs9001@gmail.comgit config --global user.email mbalazs9001@gmail.comgit config --global user.email mbalazs9001@gmail.com>
2018-07-03 12:45:38 +0200

    Created controllers branch

--- src/main/java/com/mng/hermes/api/RoomController.java
+++ src/main/java/com/mng/hermes/api/RoomController.java
@@ -1,7 +1,12 @@
 package com.mng.hermes.api;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RoomController {
+
+    @RequestMapping("/room/{id}")
+    public
+
 }
