fe7ed66dca9b620c574aca5750a463a1acf3e760
AttilaMüller <muller.attila.aron@gmail.com>
2018-07-04 12:38:32 +0200

    added logout route to controller

--- src/main/java/com/mng/hermes/api/UserController.java
+++ src/main/java/com/mng/hermes/api/UserController.java
@@ -3,6 +3,7 @@ package com.mng.hermes.api;
 import com.mng.hermes.dto.UserDTO;
 import com.mng.hermes.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,5 +15,12 @@ public class UserController {
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public UserDTO validateUser(@RequestHeader(value="Authorization") String authorization){
         return new UserDTO(userService.loginUser(authorization));
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.OK)
+    public void logoutUser(@RequestHeader(value="Authorization") String authorization){
+        userService.logoutUser(authorization);
+
     }
 }
